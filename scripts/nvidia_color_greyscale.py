bdcaecd823a1960647af6c210ceeee92b049edc7
Justin W. Flory <git@jwf.io>
2018-07-15 14:56:24 -0500

    Look ma, I have opinions about my code!
    I take a more opinionated way of writing my Python script. Specifically,
    I changed the following:
    
    * Follow all PEP-8 standards
    * Switch to single apostrophes instead of quotes for code (preserve full
      quotes for docstrings)
    * Improve comments, add links where applicable
    * Other picky changes to hopefully improve readability

--- scripts/nvidia_color_greyscale.py
+++ scripts/nvidia_color_greyscale.py
@@ -1,65 +1,74 @@
 #!/usr/bin/env python3
 """
-Enable or disable a greyscale or color profile for your Nvidia graphics card.
-The purpose of doing this is to encourage good health with daily computing
-usage. The script simplifies the user interface to making this change.
+Enable or disable greyscale color profile on systems with Nvidia graphics card.
+
+This script enables or disables a greyscale or color profile for a system using
+a Nvidia graphics card. This encourages good health for daily computing usage
+and counteracts against the psychology of color.
+
+https://blog.mozilla.org/internetcitizen/2018/02/13/grayscale/
+
+The script simplifies the user interface to making this change.
 """
 
-import argparse, re, subprocess, sys
+import argparse
+import re
+import subprocess
+import sys
 
-__author__ = "Justin W. Flory"
-__license__ = "Mozilla Public License 2.0"
+__author__ = 'Justin W. Flory'
+__license__ = 'Mozilla Public License 2.0'
+
 
 def retrieve():
     """
-    Retrieves current digital vibrance value set by nvidia driver
+    Retrieves current digital vibrance value set by nvidia driver.
 
     First, it queries setting value on the HDMI-0 display (this is hard-coded
     now, but should be self-detectable. Then, the output of the command is
     parsed and split with regex to extract the actual config value.
     """
     vibrance_readout = []
-    
-    cmd = ["nvidia-settings", "-q", "[DPY:HDMI-0]/DigitalVibrance"]
+
+    cmd = ['nvidia-settings', '-q', '[DPY:HDMI-0]/DigitalVibrance']
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     for line in proc.stdout.readlines():
         if "Attribute 'DigitalVibrance'".encode('utf-8') in line:
             vibrance_readout = re.split('[; |, |. |\*|\n]', str(line))
-            print("Current color profile: {}".format(vibrance_readout[5]))
-            print("0 means default color display.",
-                  "\n-1024 means greyscale display.")
+            print('Current color profile: {}'.format(vibrance_readout[5]))
+            print('0 means default color display.',
+                  '\n-1024 means greyscale display.')
+
 
 def set_color():
-    """
-    Sets display profile to default color profile (via nvidia-settings)
-    """
-    subprocess.call(["nvidia-settings", "-a", "[DPY:HDMI-0]/DigitalVibrance=0"])
+    """Set display profile to default color profile (via nvidia-settings)."""
+    subprocess.call(
+        ['nvidia-settings', '-a', '[DPY:HDMI-0]/DigitalVibrance=0'])
+
 
 def set_greyscale():
-    """
-    Sets display profile to greyscale profile (via nvidia-settings)
-    """
-    subprocess.call(["nvidia-settings", "-a",
-                    "[DPY:HDMI-0]/DigitalVibrance=-1024"])
+    """Set display profile to greyscale profile (via nvidia-settings)."""
+    subprocess.call(
+        ['nvidia-settings', '-a', '[DPY:HDMI-0]/DigitalVibrance=-1024'])
+
 
 def main():
-    """
-    Main function, initializes class.
-    """
+    """Main function, initializes class."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--color", "-c", help="enable default color profile (0)",
-                        action="store_true")
-    parser.add_argument("--greyscale", "-g",
-                        help="enable greyscale profile (-1024)",
-                        action="store_true")
-    parser.add_argument("--retrieve", "-r",
-                        help="retrieve existing color profile value",
-                        action="store_true")
+    parser.add_argument(
+        '--color', '-c', action='store_true',
+        help='enable default color profile (0)')
+    parser.add_argument(
+        '--greyscale', '-g', action='store_true',
+        help='enable greyscale profile (-1024)')
+    parser.add_argument(
+        '--retrieve', '-r', action='store_true',
+        help='retrieve existing color profile value')
     args = parser.parse_args()
-    
+
     if len(sys.argv) > 2:
-        print("Too many arguments specified. Please run with -h flag for help.")
-        sys.exit(1)
+        raise SystemExit(
+            "Too many arguments. Use '-h' argument for usage help.")
 
     if args.retrieve:
         retrieve()
@@ -67,6 +76,7 @@ def main():
         set_color()
     if args.greyscale:
         set_greyscale()
+
 
 if __name__ == '__main__':
     main()
