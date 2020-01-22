2dd45be6259f6ca5770221b22cc9fb91f90ac67b
Justin W. Flory <git@jwf.io>
2020-01-22 10:38:46 -0500

    :wastebasket: scripts: Discard ripright scripts/container
    Unfortunately, `ripright` is a pain to use on Fedora. I discovered
    `whipper`, which is a cool Python3 project that seems to meet the use
    case I need for ripping audio CDs. It includes FLAC support and uses the
    MusicBrainz database for metadata lookups.
    
    This commit is clearing the way for a new script or way of ripping my
    audio CDs.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- scripts/ripright.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-#
-# Script to rip a CD in the CD drive to FLAC and save it to my library in the
-# preferred naming scheme. Runs until terminated.
-#
-
-ripright -c /dev/sr1 -o "$HOME/Music/%b/%D/%N %T.flac"
-
